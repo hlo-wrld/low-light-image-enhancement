7b28c539ed7e26c8a464001e687dd2781f26c8c0
Shoilayee <125804336+hlo-wrld@users.noreply.github.com>
2024-06-12 20:52:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from method1-LIME import LIME
 def main():
   image_dir = "/test/low"
   save_dir = "/test/high"
-  image_paths = load_images(img_dir)
+  lime = LIME(iterations=30,alpha=0.15,rho=1.1,gamma=0.6,strategy=2,exact=True)
+  image_paths = lime.load_images(img_dir)
   for image_path in image_paths:
-    lime = LIME(iterations=30,alpha=0.15,rho=1.1,gamma=0.6,strategy=2,exact=True)
     lime.load(image_path)
     R = lime.run()
     base_filename = os.path.basename(image_path)
