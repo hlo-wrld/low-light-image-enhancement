e9e2d080550ed2b9d6d81d6a00c38d1ff4be6628
Shoilayee <125804336+hlo-wrld@users.noreply.github.com>
2024-06-12 20:11:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
+import cv2
+from method1-LIME import LIME
 
+def main():
+  filePath = "./pics/2.jpg"
+  lime = LIME(iterations=30,alpha=0.15,rho=1.1,gamma=0.6,strategy=2,exact=True)
+  lime.load(filePath)
+  R = lime.run()
+  savePath = "/kaggle/working/pics_dual_enh.png"
+  cv2.imwrite(savePath, R)
