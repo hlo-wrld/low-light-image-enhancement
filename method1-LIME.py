e309eb29f0351303bbffeda569ba8d1aa802c1de
Shoilayee <125804336+hlo-wrld@users.noreply.github.com>
2024-06-12 20:09:27 +0530

    Update method1-LIME.py

--- method1-LIME.py
+++ method1-LIME.py
@@ -1,1 +1,95 @@
+import numpy as np
+from scipy.fft import *
+from skimage import exposure
+import cv2
+from tqdm import trange
 
+class LIME:
+  def __init__(self, iterations, alpha, rho, gamma, strategy, exact):
+        self.iterations = iterations
+        self.alpha = alpha
+        self.rho = rho
+        self.gamma = gamma
+        self.strategy = strategy
+        self.exact = exact
+
+    def load(self, imgPath):
+        self.loadimage(cv2.imread(imgPath) / 255)
+
+    def loadimage(self,L):
+        self.L = L
+        self.row = self.L.shape[0]
+        self.col = self.L.shape[1]
+
+        self.T_esti = np.max(self.L, axis=2)
+        self.Dv = -np.eye(self.row) + np.eye(self.row, k=1)
+        self.Dh = -np.eye(self.col) + np.eye(self.col, k=-1)
+
+        dx = np.zeros((self.row, self.col))
+        dy = np.zeros((self.row, self.col))
+        dx[1, 0] = 1
+        dx[1, 1] = -1
+        dy[0, 1] = 1
+        dy[1, 1] = -1
+        dxf = fft2(dx)
+        dyf = fft2(dy)
+        self.DTD = np.conj(dxf) * dxf + np.conj(dyf) * dyf
+
+        self.W = self.Strategy()
+
+    def Strategy(self):
+        if self.strategy == 2:
+            self.Wv = 1 / (np.abs(self.Dv @ self.T_esti) + 1)
+            self.Wh = 1 / (np.abs(self.T_esti @ self.Dh) + 1)
+            return np.vstack((self.Wv, self.Wh))
+        else:
+            return np.ones((self.row * 2, self.col))
+
+    def T_sub(self, G, Z, miu):
+        X = G - Z / miu
+        Xv = X[:self.row, :]
+        Xh = X[self.row:, :]
+
+        numerator = fft2(2 * self.T_esti + miu * (self.Dv @ Xv + Xh @ self.Dh))
+        denominator = self.DTD * miu + 2
+        T = np.real(ifft2(numerator / denominator))
+
+        return exposure.rescale_intensity(T, (0, 1), (0.001, 1))
+
+    def G_sub(self, T, Z, miu, W):
+        epsilon = self.alpha * W / miu
+        temp = np.vstack((self.Dv @ T, T @ self.Dh)) + Z / miu
+        return np.sign(temp) * np.maximum(np.abs(temp) - epsilon, 0)
+
+    def Z_sub(self, T, G, Z, miu):
+        return Z + miu * (np.vstack((self.Dv @ T, T @ self.Dh)) - G)
+
+    def miu_sub(self, miu):
+        return miu * self.rho
+
+    def run(self):
+        if self.exact:
+            T = np.zeros((self.row, self.col))
+            G = np.zeros((self.row * 2, self.col))
+            Z = np.zeros((self.row * 2, self.col))
+            miu = 1
+
+            for i in trange(0,self.iterations):
+                T = self.T_sub(G, Z, miu)
+                G = self.G_sub(T, Z, miu, self.W)
+                Z = self.Z_sub(T, G, Z, miu)
+                miu = self.miu_sub(miu)
+
+            self.T = T ** self.gamma
+            self.R = self.L / np.repeat(self.T[..., None], 3, axis = -1)
+            return exposure.rescale_intensity(self.R,(0,1)) * 255
+        # TODO: rapid algorithm
+        else:
+            pass
+    def psnr(original, compressed): 
+      mse = np.mean((original - compressed) ** 2) 
+      abs_er = np.abs(original - compressed)
+      mae = np.mean(abs_er)
+      max_pixel = 255.0
+      psnr = 20 * log10(max_pixel / sqrt(mse)) 
+      print ("psnr: ",psnr,'mse: ',mse,'mae: ',mae)
